10abd711a2b6e0ae03f45392efbb2cbd4a10577a
sahil <sahil@zengamedia.com>
2025-03-06 16:15:15 +0530

    Initial setup for E-commerce API

---
+++ src/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+    res.send("E-commerce API is running...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
